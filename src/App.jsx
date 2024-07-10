0ce53e476adc3951ef91a80ca83aa796edc28780
Himanshu001-U <himanshuupadhayay2021@gmail.com>
2024-07-10 14:33:42 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Body from './components/body'
+import Footer from './components/footer'
+import Header from './components/header'
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+    
+  )
+}
+
+export default App
